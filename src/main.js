77ee50a5826140b7b24abc1a3b256a9ae4452619
junsoo875 <junsoo875@gmail.com>
2020-06-08 20:35:05 +0900

    First:GoogleMap Apply

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import router from '@/router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import router from "@/router";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
-    router,
-    render: h => <router-view/>
-}).$mount('#app')
+  router,
+  vuetify,
+  render: (h) => <router-view />,
+}).$mount("#app");
--- src/routes/index.vue
+++ src/routes/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    인덱스 라우트 입니다.<span uk-icon="robot"></span>
+  <div id="map" style="width:100%; height: 100vh;">
+    <script
+      type="application/javascript"
+      async
+      defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEUyBvbBDrDO4i9dBXV8nzwPZynVlxXgE&callback=initMap&region=kr"
+    ></script>
+
+    <span uk-icon="robot"></span>
   </div>
 </template>
+
+<script type="application/javascript">
+window.onload = function initMap() {
+  var seoul = { lat: 37.5642135, lng: 127.0016985 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: seoul,
+  });
+};
+</script>
