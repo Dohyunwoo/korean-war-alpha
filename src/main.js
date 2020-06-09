f75296a0930184344d7596a9ce250e2846056fbb
Dohyunwoo <ehgusfn12@gmail.com>
2020-06-09 15:00:03 +0900

    지도 크기 수정 및 api fetch 결과 랜더

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import router from "@/router";
 import vuetify from "./plugins/vuetify";
+import VueCompositionApi from '@vue/composition-api'
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(VueCompositionApi);
 
 new Vue({
   router,
