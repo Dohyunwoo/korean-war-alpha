42432fd8cfc3f8acaa7b6bac89e87f7a9b0e6d02
Dohyunwoo <ehgusfn12@gmail.com>
2020-07-11 01:00:19 +0900

    잘못된 이미지 경로 수정 및 파비콘 이미지 추가

--- src/routes/patriotic.vue
+++ src/routes/patriotic.vue
@@ -39,31 +39,31 @@
       <div>
         <div class="uk-card uk-card-hover uk-card-body">
           <h3 class="uk-card-title">태극무공훈장</h3>
-          <img src="/images/honor/태극무공훈장.png"alt="미국 은성훈장">
+          <img src="/images/honor/taeguek-medal.png"alt="태극무공훈장">
         </div>
       </div>
       <div>
         <div class="uk-card uk-card-hover uk-card-body">
           <h3 class="uk-card-title">을지무공훈장</h3>
-          <img src="/images/honor/을지무공훈장.png"alt="을지무공훈장">
+          <img src="/images/honor/eulji-medal.png"alt="을지무공훈장">
         </div>
       </div>
       <div>
         <div class="uk-card uk-card-hover uk-card-body">
           <h3 class="uk-card-title">충무무공훈장</h3>
-          <img src="/images/honor/충무무공훈장.png"alt="충무무공훈장">
+          <img src="/images/honor/chungmu-medal.png"alt="충무무공훈장">
         </div>
       </div>
       <div>
         <div class="uk-card uk-card-hover uk-card-body">
           <h3 class="uk-card-title">화랑무공훈장</h3>
-          <img src="/images/honor/화랑무공훈장.png"alt="화랑무공훈장">
+          <img src="/images/honor/hwarang-medal.png"alt="화랑무공훈장">
         </div>
       </div>
       <div>
         <div class="uk-card uk-card-hover uk-card-body">
           <h3 class="uk-card-title">보국통일장</h3>
-          <img src="/images/honor/보국통일장.png" alt="보국통일장">
+          <img src="/images/honor/tongil-medal.png" alt="보국통일장">
         </div>
       </div>
     </div>
